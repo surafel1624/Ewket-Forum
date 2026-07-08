58b7299bc39b626fb05ccb2cc771e57061d9c4d1
Surafel <surafelsu@gmail.com>
2026-07-08 21:53:45 +0300

    Set up .env file

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,11 +1,11 @@
 const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
-    user: "root",
-    database: "ewket_forum",
-    host: "localhost",
-    password: "",
-    connectionLimit: 10
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10
 });
 
 module.exports = dbConnection.promise();
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,7 @@ async function authMiddleware (req, res, next){
     }
     const token = authHeader.split(' ')[1];
     try {
-        const {username, userid} = jwt.verify(token, "secret");
+        const {username, userid} = jwt.verify(token, process.env.JWT_SECRET);
         req.user = {username, userid};
         next();
     } catch (error) {
