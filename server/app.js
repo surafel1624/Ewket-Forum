69604062de00cb1b0dfdf61c27e15f8ffac1dac3
Surafel <surafelsu@gmail.com>
2026-07-20 21:11:48 +0300

    Fix CORS policy for Netlify deployment and local testing

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const questionRoutes = require('./routes/questionRoute');
 const authMiddleware = require('./middlewares/auth');
 const cors = require('cors');
 
-app.use(cors({origin: 'https://ewketforum.netlify.app/'}));
+app.use(cors({origin: ['https://ewketforum.netlify.app', 'http://localhost:5173'], credentials: true}));
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/questions", authMiddleware, questionRoutes);
